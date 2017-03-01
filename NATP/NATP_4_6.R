43929c12ad2c35faf00a099df19b64abc1ac7893
davidluizrusso <david.luiz.russo@gmail.com>
2017-02-28 20:51:03 -0800

    Added LR, LDA, QDA predictions. Begin work on KNN predictions

--- NATP/NATP_4_6.R
+++ NATP/NATP_4_6.R
@@ -25,6 +25,11 @@ Smarket_train <- predict(preProc_train, Smarket_train)
 preProc_test <- preProcess(Smarket_test)
 Smarket_test <- predict(preProc_test, Smarket_test)
 
+# get KNN data sets
+Smarket_train_x <- Smarket_train[, 1:6]
+Smarket_test_x <- Smarket_test[, 1:6]
+Smarket_train_y <- Smarket_test[, "Direction"]
+
 #----------------------------------- Exploratory data analysis
 
 # Correlations
@@ -45,9 +50,10 @@ Smarket_train %>%
 model_paster <- function(cans, p){
   
   combs <- as.data.frame(t(combn(cans, p)))
-  mods <- apply(combs, 1, paste, collapse = " ~ ")
+  mods <- apply(combs, 1, paste, collapse = " + ")
   mods <- as.data.frame(paste0("Direction ~ ", mods))
   names(mods) <- "model"
+  mods$model <- as.character(mods$model)
   mods
   
 }
@@ -56,9 +62,143 @@ candidates <- c("Lag1", "Lag2", "Lag3", "Lag4", "Lag5", "Volume")
 
 all_models <- do.call(rbind,
                      lapply(seq_along(candidates),
-                     function(x) model_paster(cans = candidates, p = x)))
+                     function(x) model_paster(cans = candidates, p = x))) 
+
+knn_models <- expand.grid(c(TRUE, FALSE),
+                          c(TRUE, FALSE),
+                          c(TRUE, FALSE),
+                          c(TRUE, FALSE),
+                          c(TRUE, FALSE),
+                          c(TRUE, FALSE)
+                          )
+
+knn_models <- knn_models[-1, ]
+
+# get KNN data sets
+Smarket_train_x <- Smarket_train[, 1:6]
+Smarket_test_x <- Smarket_test[, 1:6]
+Smarket_train_y <- Smarket_train[, "Direction"]
+
+
+knn_preds <- 
+lapply(1:nrow(knn_models), function(x) knn(data.frame(Smarket_train_x[, as.logical(knn_models[x, ])]),
+                                           data.frame(Smarket_test_x[, as.logical(knn_models[x, ])]),
+                                           Smarket_train_y,
+                                           k = 1))
 
 #----------------------------------- Write model fitting functions for LR, LDA, and QDA
+
+fit_glm <- function(formula, train_data, test_data, response_var){
+  
+  glm_candidate <- glm(as.formula(formula),
+                       data = train_data,
+                       family = binomial)
+  
+  glm.preds <- ifelse(predict(glm_candidate, newdata = test_data, type = "response") < 0.5, 
+                      "Down",
+                      "Up")
+  
+  acc <- round(mean(glm.preds == test_data[, response_var]), 4)
+  
+  true_pos <- round(sum(glm.preds == "Up" & test_data[, response_var] == "Up")/
+                    sum(test_data[, response_var] == "Up"), 4)
+  
+  true_neg <- round(sum(glm.preds == "Down" & test_data[, response_var] == "Down")/
+                      sum(test_data[, response_var] == "Down"), 4)
+  
+  res <- data.frame(model = "logistic regression",
+                    variables = formula,
+                    accuracy = acc,
+                    `true positive rate` = true_pos,
+                    `true negative rate` = true_neg)
+  
+  res
+  
+}
+
+
+fit_lda <- function(formula, train_data, test_data, response_var){
+  
+  lda_candidate <- lda(as.formula(formula),
+                       data = train_data)
+  
+  lda.preds <- predict(lda_candidate, newdata = test_data)$class
+  
+  acc <- round(mean(lda.preds == test_data[, response_var]), 4)
+  
+  true_pos <- round(sum(lda.preds == "Up" & test_data[, response_var] == "Up")/
+                      sum(test_data[, response_var] == "Up"), 4)
+  
+  true_neg <- round(sum(lda.preds == "Down" & test_data[, response_var] == "Down")/
+                      sum(test_data[, response_var] == "Down"), 4)
+  
+  res <- data.frame(model = "lda",
+                    variables = formula,
+                    accuracy = acc,
+                    `true positive rate` = true_pos,
+                    `true negative rate` = true_neg)
+  
+  res
+  
+}
+
+fit_qda <- function(formula, train_data, test_data, response_var){
+  
+  qda_candidate <- qda(as.formula(formula),
+                       data = train_data)
+  
+  qda.preds <- predict(qda_candidate, newdata = test_data)$class
+  
+  acc <- round(mean(qda.preds == test_data[, response_var]), 4)
+  
+  true_pos <- round(sum(qda.preds == "Up" & test_data[, response_var] == "Up")/
+                      sum(test_data[, response_var] == "Up"), 4)
+  
+  true_neg <- round(sum(qda.preds == "Down" & test_data[, response_var] == "Down")/
+                      sum(test_data[, response_var] == "Down"), 4)
+  
+  res <- data.frame(model = "qda",
+                    variables = formula,
+                    accuracy = acc,
+                    `true positive rate` = true_pos,
+                    `true negative rate` = true_neg)
+  
+  res
+  
+}
+
+
+
+glms <- do.call(rbind,
+                lapply(seq_along(t(all_models)), function(x) fit_glm(formula = all_models$model[x],
+                                                                     train_data = Smarket_train,
+                                                                     test_data = Smarket_test,
+                                                                     response_var = "Direction"
+                                                                     )
+                       )
+                )
+
+ldas <- do.call(rbind,
+                lapply(seq_along(t(all_models)), function(x) fit_lda(formula = all_models$model[x],
+                                                                     train_data = Smarket_train,
+                                                                     test_data = Smarket_test,
+                                                                     response_var = "Direction"
+                )
+                )
+)
+
+qdas <- do.call(rbind,
+                lapply(seq_along(t(all_models)), function(x) fit_qda(formula = all_models$model[x],
+                                                                     train_data = Smarket_train,
+                                                                     test_data = Smarket_test,
+                                                                     response_var = "Direction"
+                )
+                )
+)
+
+all_fit_models <-
+  do.call(rbind, list(glms, ldas, qdas)) %>%
+  dplyr::arrange(desc(accuracy), desc(true.positive.rate), desc(true.negative.rate))
 
 
           
